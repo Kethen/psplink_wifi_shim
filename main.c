54d20927edf0288df9fec0f2238a2f6c94ac8c9d
Katharine Chui <katharine.chui@gmail.com>
2026-05-01 11:56:30 +0200

    make use of sysclib string formatting in printf hook, some cleanup

--- main.c
+++ main.c
@@ -20,6 +20,7 @@
 #include <psputils.h>
 #include <psploadcore.h>
 #include <pspsysmem_kernel.h>
+#include <pspsysclib.h>
 
 #include <systemctrl.h>
 
@@ -49,231 +50,55 @@
 
 PSP_MODULE_INFO("psplink_wifi_shim", PSP_MODULE_KERNEL, 1, 0);
 
-// vsnprintf from aemu, likely by mrcoldbird, or imported from somewhere by mrcoldbird
-static int itostr(char *buf, int in_data, int base, int upper, int sign)
-{
-	int res, len, i;
-	unsigned int data;
-	char *str;
 
-	if(base==10 && sign && in_data<0){
-		data = -in_data;
-	}else{
-		data = in_data;
+struct _vsnprintf_ctx {
+	int max_len;
+	int offset;
+	int cnt;
+	char *buf;
+};
+
+static void _vsnprintf_callback(void *_ctx, int c){
+	struct _vsnprintf_ctx *ctx = _ctx;
+	if (c == 0x200){
+		return;
 	}
 
-	str = buf;
-	do{
-		res = data%base;
-		data = data/base;
-		if(res<10){
-			res += '0';
-		}else{
-			if(upper){
-				res += 'A'-10;
-			}else{
-				res += 'a'-10;
-			}
+	if (c == 0x201){
+		if (ctx->offset < ctx->max_len){
+			ctx->buf[ctx->offset] = '\0';
+			ctx->offset++;
 		}
-		*str++ = res;
-	}while(data);
-	len = str-buf;
-
-	/* reverse digital order */
-	for(i=0; i<len/2; i++){
-		res = buf[i];
-		buf[i] = buf[len-1-i];
-		buf[len-1-i] = res;
+		return;
 	}
 
-	return len;
+	ctx->cnt++;
+
+	if (ctx->offset >= ctx->max_len){
+		return;
+	}
+
+	if (ctx->offset == ctx->max_len - 1){
+		ctx->buf[ctx->offset] = '\0';
+		ctx->offset++;
+		return;
+	}
+
+	ctx->buf[ctx->offset] = c;
+	ctx->offset++;
+	return;
 }
 
-#define OUT_C(c) \
-if(str<end){ \
-	*str++ = (c); \
-} else { \
-	goto exit; \
-}
-static int _vsnprintf(char *buf, size_t size, const char *fmt, va_list args)
-{
-	static char digital_buf[32] = {0};
-	char ch, *s, *str, *end, *sstr;
-	int zero_pad, left_adj, add_sign, field_width, sign;
-	int i, base, upper, len;
+static int _vsnprintf(char *buf, int len, const char *fmt, va_list args){
+	struct _vsnprintf_ctx ctx = {
+		.max_len = len,
+		.offset = 0,
+		.buf = buf
+	};
 
-	if(!buf || !fmt ||!size){
-		return 0;
-	}
+	prnt(_vsnprintf_callback, &ctx, fmt, args);
 
-	str = buf;
-	end = buf+size;
-
-	while(*fmt){
-		if(*fmt!='%'){
-			OUT_C(*fmt++);
-			continue;
-		}
-
-		/* skip '%' */
-		sstr = (char *)fmt;
-		fmt++;
-
-		/* %% */
-		if(*fmt=='%'){
-			OUT_C(*fmt++);
-			continue;
-		}
-
-		/* get flag */
-		zero_pad = ' ';
-		left_adj = 0;
-		add_sign = 0;
-		while((ch=*fmt)){
-
-			if(*fmt=='0'){
-				zero_pad = '0';
-			}else if(*fmt=='-'){
-				left_adj = 1;
-			}else if(*fmt=='#'){
-			}else if(*fmt==' '){
-				if(add_sign!='+')
-					add_sign = ' ';
-			}else if(*fmt=='+'){
-				add_sign = '+';
-			}else{
-				break;
-			}
-			fmt++;
-		}
-
-		/* get field width: m.n */
-		field_width = 0;
-		/* get m */
-		while(*fmt && *fmt>'0' && *fmt<='9'){
-			field_width = field_width*10+(*fmt-'0');
-			fmt++;
-		}
-		if(*fmt && *fmt=='.'){
-			fmt++;
-			/* skip n */
-			while(*fmt && *fmt>'0' && *fmt<='9'){
-				fmt++;
-			}
-		}
-
-		/* get format char */
-		upper = 0;
-		base = 0;
-		sign = 0;
-		len = 0;
-		s = digital_buf;
-		while((ch=*fmt)){
-			fmt++;
-			switch(ch){
-				/* hexadecimal */
-				case 'p':
-				case 'X':
-					upper = 1;
-				case 'x':
-					base = 16;
-					break;
-
-					/* decimal */
-					case 'd':
-					case 'i':
-						sign = 1;
-					case 'u':
-						base = 10;
-						break;
-
-						/* octal */
-						case 'o':
-							base = 8;
-							break;
-
-							/* character */
-							case 'c':
-								digital_buf[0] = (unsigned char) va_arg(args, int);
-								len = 1;
-								break;
-
-								/* string */
-								case 's':
-									s = va_arg(args, char *);
-									if(!s) s = "<NUL>";
-									len = strlen(s);
-				break;
-
-				/* float format, skip it */
-				case 'e': case 'E': case 'f': case 'F': case 'g': case 'G': case 'a': case 'A':
-					va_arg(args, double);
-					s = NULL;
-					break;
-
-					/* length modifier */
-					case 'l': case 'L': case 'h': case 'j': case 'z': case 't':
-						/* skip it */
-						continue;
-
-						/* bad format */
-						default:
-							s = sstr;
-							len = fmt-sstr;
-							break;
-			}
-			break;
-		}
-
-		if(base){
-			i = va_arg(args, int);
-			if(base==10 && sign){
-				if(i<0){
-					add_sign = '-';
-				}
-			}else{
-				add_sign = 0;
-			}
-
-			len = itostr(digital_buf, i, base, upper, sign);
-		}else{
-			zero_pad = ' ';
-			add_sign = 0;
-		}
-
-		if(s){
-			if(len>=field_width){
-				field_width = len;
-				if(add_sign)
-					field_width++;
-			}
-			for(i=0; i<field_width; i++){
-				if(left_adj){
-					if(i<len){
-						OUT_C(*s++);
-					}else{
-						OUT_C(' ');
-					}
-				}else{
-					if(add_sign && (zero_pad=='0' || i==(field_width-len-1))){
-						OUT_C(add_sign);
-						add_sign = 0;
-						continue;
-					}
-					if(i<(field_width-len)){
-						OUT_C(zero_pad);
-					}else{
-						OUT_C(*s++);
-					}
-				}
-			}
-		}
-	}
-
-	OUT_C(0);
-
-	exit:
-	return str-buf;
+	return ctx.cnt;
 }
 
 static int is_vita(){
@@ -541,9 +366,9 @@ static void log_memory_info_wifi(){
 		if (query_status == 0){
 			int max_free = sceKernelPartitionMaxFreeMemSize(i);
 			int total_free = sceKernelPartitionTotalFreeMemSize(i);
-			len = sprintf(buf, "%s: p%d startaddr 0x%x size %d attr 0x%x max %d total %d\n", __func__, i, meminfo.startaddr, meminfo.memsize, meminfo.attr, max_free, total_free);
+			len = sprintf(buf, "p%d startaddr 0x%x size %d attr 0x%x max %d total %d\n", i, meminfo.startaddr, meminfo.memsize, meminfo.attr, max_free, total_free);
 		}else{
-			len = sprintf(buf, "%s: p%d query failed, 0x%x\n", __func__, i, query_status);
+			len = sprintf(buf, "p%d query failed, 0x%x\n", i, query_status);
 		}
 		fill_buffer(buf, len);
 	}
@@ -603,8 +428,8 @@ int send_input_to_psplink_thread_func(unsigned int args, void *arg){
 		if (strcmp(to_psplink_input, "help") == 0){
 			static char buf[256];
 			int len = sprintf(buf, "%s", "psplink_wifi_shim extra commands:\n\n");
-			len += sprintf(&buf[len], "%s", "  meminfo2 - meminfo with more partitions\n");
-			len += sprintf(&buf[len], "%s", "  exit2 - run sctrlKernelExitVSH(NULL), useful in Adrenaline\n\n");
+			len += sprintf(&buf[len], "%-10s - %s\n", "meminfo2", "meminfo with more partitions");
+			len += sprintf(&buf[len], "%-10s - %s\n\n", "exit2", "runs sctrlKernelExitVSH(NULL) to reboot to VSH");
 			fill_buffer(buf, len);
 		}
 
@@ -712,6 +537,7 @@ static int printf_patched(const char *fmt, ...){
 	va_start(args, fmt);
 	int len = _vsnprintf(buf, sizeof(buf), fmt, args);
 	va_end(args);
+	len = len > sizeof(buf) - 1 ? sizeof(buf) - 1 : len;
 	#if 1
 	if (buffer_consumer != NULL){
 		//buffer_consumer(buf, len);
